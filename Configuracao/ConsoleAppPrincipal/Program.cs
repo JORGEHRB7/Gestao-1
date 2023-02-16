6fb3c418ffa2910f57fabc9c7ce314a653a04989
implementacaoteste <106354515+implementacaoteste@users.noreply.github.com>
2023-02-15 22:06:30 -0300

    Concluído o teste de inserção no banco

--- Configuracao/ConsoleAppPrincipal/Program.cs
+++ Configuracao/ConsoleAppPrincipal/Program.cs
@@ -5,15 +5,22 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Usuario usuario = new Usuario();
-        usuario.Nome = "Erisvaldo Carvalho Silva";
-        usuario.NomeUsuario = "erisvaldo";
-        usuario.Ativo = true;
-        usuario.Email = "contato@erisvaldocarvalho.com.br";
-        usuario.CPF = "458.158.442-88";
-        usuario.Senha = "123";
+        try
+        {
+            Usuario usuario = new Usuario();
+            UsuarioBLL usuarioBLL = new UsuarioBLL();
+            usuario.Nome = "Erisvaldo Carvalho Silva";
+            usuario.NomeUsuario = "erisvaldo";
+            usuario.Ativo = true;
+            usuario.Email = "contato@erisvaldocarvalho.com.br";
+            usuario.CPF = "458.158.442-88";
+            usuario.Senha = "504587545";
 
-        UsuarioBLL usuarioBLL = new UsuarioBLL();
-        usuarioBLL.Inserir(usuario);
+            usuarioBLL.Inserir(usuario);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 }
